a1ff99bd6b75676a720d01d60c0850ceada37a7d
Alpha <alphatero@icloud.com>
2021-11-30 23:28:00 +0800

    add fetch to line api

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -96,15 +96,6 @@ export default {
         }
       } catch (error) {}
     },
-    validateEmail(email) {
-      const reg =
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      return reg.test(String(email).toLowerCase())
-    },
-    validatePassword(password) {
-      const reg = /^(?=.*\d)(?=.*[a-zA-Z]).{6,30}$/
-      return reg.test(String(password))
-    },
     linkLineSignIn() {},
   },
 }
