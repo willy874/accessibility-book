2b7f9783b687bd8dbcad97f13106900de006de80
willy <willy8742891@gmail.com>
2022-01-06 01:56:39 +0800

    fix: server router *

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const port = 8001
 app.use(express.static(path.resolve(process.cwd(), 'dist')))
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   const file = fs.readFileSync(path.resolve(process.cwd(), 'dist', 'index.html'))
   res.send(file.toString())
 })
