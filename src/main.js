84ec613c2dce5cdf5f3c5dd903c9368f0a147b3f
willy <willy8742891@gmail.com>
2022-02-13 22:51:13 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ async function main() {
   const LiifQuery = new URLSearchParams(location.search.substring(1)).get('liff.state')
   if (Config.value.liff || LiifQuery) {
     await liff.init({ liffId: Config.value.liffId })
-    const route = router.resolve(LiifQuery).route
-    await router.replace(route)
+    // const route = router.resolve(LiifQuery).route
+    // await router.replace(route)
   }
   const app = new Vue({
     router,
