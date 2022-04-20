3317eaf1802e55f85a89c932ed11e2989d273327
haze3161798 <haze3161798@gmail.com>
2022-04-20 20:28:23 +0800

    功能做完了 註解還沒寫

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,25 +1,49 @@
 <template>
-  <div></div>
+  <div>
+    <h2>搜尋列表</h2>
+    <ul v-if="resData">
+      <li v-for="searchList in resData" :key="searchList.id" class="serch-list" @click="linkTo(searchList.id)">
+        {{ searchList.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import { apiPostSearch } from '@/api'
+import { handleHttpErrorLog } from '@/utils'
 import Config from '@/config'
 export default {
   name: 'Search',
+  data() {
+    return {
+      resData: null,
+    }
+  },
   methods: {
+    linkTo(link) {
+      this.$router.push(`book/${link}`)
+    },
     async effectRoute() {
       const body = Config.getRoute(this).query
-      console.log(body)
       try {
-        const res = await apiPostSearch({ content: body })
-        console.log(res)
+        const res = await apiPostSearch(body)
+        this.resData = res.data.results
       } catch (error) {
-        console.log(error)
+        handleHttpErrorLog(error)
       }
     },
   },
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.serch-list {
+  cursor: pointer;
+  font-size: 18px;
+  padding: 8px;
+}
+h2 {
+  margin-bottom: 8px;
+}
+</style>
