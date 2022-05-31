5bbdf3b00ee3a25a8ef8e80892bed6eeffeb7127
haze3161798 <haze3161798@gmail.com>
2022-05-31 23:11:47 +0800

    改 logo, 首頁圖 驗證做到一半

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,7 +1,9 @@
 <template>
   <header>
     <SearchBar>
-      <router-link title="回到首頁" :to="{ name: RouterName.HOME }">首頁</router-link>
+      <router-link title="回到首頁" :to="{ name: RouterName.HOME }">
+        <img class="header-logo" src="https://eyestudy.org/media/static/Logo.jpg" alt="" />
+      </router-link>
     </SearchBar>
     <nav>
       <ul v-if="menuList.length">
@@ -173,6 +175,9 @@ header {
         color: white;
       }
     }
+  }
+  .header-logo {
+    width: 30px;
   }
 }
 </style>
--- src/views/RevisePassword.vue
+++ src/views/RevisePassword.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="revise-password-wrapper">
-    <form @submit.prevent="revisePassword">
+    <form @submit.prevent="validatePassword">
       <div class="form-item">
-        <lable>密碼</lable>
+        <label>密碼</label>
         <input v-model="password.newPassword" title="請輸入密碼" placeholder="請輸入密碼" type="password" />
+        <small>{{}}</small>
       </div>
       <div class="form-item">
-        <lable>確認密碼</lable>
+        <label>確認密碼</label>
         <input v-model="password.checkPassword" title="請輸入確認密碼" placeholder="請輸入確認密碼" type="password" />
       </div>
       <div class="revise-password-submit-btn">
@@ -18,6 +19,8 @@
 
 <script>
 import { apiPostPasswordRegister } from '@/api/index'
+import { ValidateType } from '@/consts'
+import { validate } from '@/utils'
 export default {
   data() {
     return {
@@ -28,6 +31,24 @@ export default {
     }
   },
   methods: {
+    async validatePassword() {
+      const form = {
+        password1: this.password.newPassword,
+        password2: this.password.checkPassword,
+      }
+      const result = await validate(form, {
+        password1: {
+          [ValidateType.IS_EMPTY]: { message: '請填寫密碼' },
+          [ValidateType.PASSWORD]: { min: 6, max: 30 },
+        },
+        password2: {
+          [ValidateType.IS_EMPTY]: { message: '請填寫確認密碼' },
+          [ValidateType.EQUAL]: { message: '請確認填寫密碼是否相等', equal: form.password1 },
+        },
+      })
+      console.log(result)
+      return result
+    },
     async revisePassword() {
       const data = {
         new_password1: this.password.newPassword,
